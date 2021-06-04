eb022e966aa3de5135f0272a7d12c12e6e35ce07
DanielKovachev <danielkv8@gmail.com>
2021-06-04 10:49:10 +0300

    Add More Params and refactor

--- train_with_feature_maps.py
+++ train_with_feature_maps.py
@@ -34,8 +34,8 @@ if __name__ == "__main__":
     parser.add_argument('--logger_file', help='output file for loggings')
     parser.add_argument('--model_save_path', help='path for model saving')
     parser.add_argument('--stats_file', help='path for tensor board')
-    #parser.add_argument('--model_class', help='model class')
-
+    parser.add_argument('--frames', type=int, help='number of frames in video')
+    parser.add_argument('--fps', type=int, help='fps for video')
 
     args = parser.parse_args()
 
@@ -47,11 +47,10 @@ if __name__ == "__main__":
     batch_size = args.batch_size
     num_processes = args.num_processes
     logging_output_file = args.logger_file
-    #hidden_dim = args.hidden_dim
-    #clip = args.clip
     stats_file = args.stats_file
     prefix_path = args.model_save_path
-    #model_class = getattr(import_module('models.{}'.format(args.model_class.lower())), args.model_class)
+    frames = args.frames
+    fps = args.fps
     lr_list = [0.0001, 0.00005]
     clip_list = [0.5, 0.1]
     hidden_dim_list = [2048, 4096]
@@ -59,7 +58,7 @@ if __name__ == "__main__":
     logging.basicConfig(filename=logging_output_file, filemode='w', format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
 
     train = []
-    with open(os.path.join(labels_folder, '1fps_100frames_train.pkl'), 'rb') as f:
+    with open(os.path.join(labels_folder, '{}fps_{}frames_train.pkl'.format(fps, frames)), 'rb') as f:
         train = pickle.load(f)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -71,12 +70,13 @@ if __name__ == "__main__":
     logging.info('start training')
     best_model_path = None
     for lr, hidden_dim, clip in itertools.product(lr_list, hidden_dim_list, clip_list):
-        for train_name_index, val_name_index in kf.split(train_dataset):
-            current_train, current_val = train_dataset[train_name_index], train_dataset[val_name_index]
-            new_train = DataLoader(current_train, shuffle=True)
-
+        for index , (train_index, val_index) in enumerate(kf.split(train_dataset)):
+            current_train = [train_dataset[i] for i in train_index]
+            current_val = [current_val[i] for i in val_index]
+            current_train = DataLoader(current_train, shuffle=True, batch_size=1)
             current_val = DataLoader(current_val, shuffle=True, batch_size=1)
-            comment = f'gradient_clip = {clip} lr = {lr} hidden_dim = {hidden_dim} ephochs = {epochs}'
+
+            comment = f'gradient_clip = {clip} lr = {lr} hidden_dim = {hidden_dim} ephochs = {epochs} cross_val = {index}'
             tb = SummaryWriter(log_dir=os.path.join(stats_file, comment))
             model = LSTM_Resnet_Model(2, hidden_dim=hidden_dim)
             model.to(device)
@@ -85,7 +85,7 @@ if __name__ == "__main__":
             optimizer = optim.Adam(model.parameters(), lr=lr)
             model.train(True)
             best_accuracy = 0
-            logging.info('stating with hypter params , lr {} , hidden_dim {} , clip {}'.format(lr, hidden_dim, clip))
+            logging.info('stating with hypter params , lr {} , hidden_dim {} , clip {}, cross_val = {}'.format(lr, hidden_dim, clip, index))
             model_path = '{}-{}-{}-{}.model'.format(prefix_path, lr, hidden_dim, clip)
             for epoch in range(epochs):  # loop over the dataset multiple times
                 logging.info('starting epoch {}'.format(epoch))
