3f75d6bf6f2fbf1f1c4a1d65e4e6f1719cdb8e01
DanielKovachev <danielkv8@gmail.com>
2021-06-18 04:01:30 +0300

    Fix Name Bug

--- pytorch-video/train.py
+++ pytorch-video/train.py
@@ -8,9 +8,10 @@ from pytorch_lightning.loggers import TensorBoardLogger
 def train(min_ephocs, dataset_folder, batch_size, num_workers, stats_file, clip_duration, model_save_dir, subsampled_frames, learning_rate):
     model = VideoClassificationLightningModule(learning_rate)
     data_module = UCFCrimeDataModule(dataset_folder, clip_duration, batch_size, num_workers, subsampled_frames)
+    base_filename = f'resnet-3d-{clip_duration}-{subsampled_frames}'
     checkpoint_callback = ModelCheckpoint(monitor='val_loss',
                         dirpath=f"{model_save_dir}",
-                        filename='resnet-3d-{clip_duration}-{subsampled_frames}-{epoch:02d}-{val_loss:.2f}'.format(clip_duration=clip_duration, subsampled_frames=subsampled_frames),
+                        filename= base_filename + '-{epoch:02d}-{val_loss:.2f}',
                         save_top_k=3,
                         mode='min')
     logger = TensorBoardLogger(stats_file, name=f"resnet-3d-{clip_duration}-{subsampled_frames}-{learning_rate}")
