0f80a8596d15a3e09f874e15758da400550e0f0f
danikv <danielkv8@gmail.com>
2021-07-20 00:00:22 +0300

    Add Clip

--- pytorch-video/train_features.py
+++ pytorch-video/train_features.py
@@ -5,17 +5,17 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 import argparse
 from pytorch_lightning.loggers import TensorBoardLogger
   
-def train(min_ephocs, dataset_folder, batch_size, num_workers, stats_file, clip_duration, model_save_dir, subsampled_frames, learning_rate, check_val_every_n_epoch, anomaly_classification, hidden_dim):
+def train(min_ephocs, dataset_folder, batch_size, num_workers, stats_file, clip_duration, model_save_dir, subsampled_frames, learning_rate, check_val_every_n_epoch, anomaly_classification, hidden_dim, gradient_clip):
     model = VideoClassificationLightningModule(learning_rate, anomaly_classification, hidden_dim)
     data_module = UCFCrimeFeatureDataModule(dataset_folder, batch_size, num_workers)
-    base_filename = f'resnet-3d-lstm-{clip_duration}-{subsampled_frames}'
+    base_filename = f'resnet-3d-lstm-{clip_duration}-{subsampled_frames}-{hidden_dim}'
     checkpoint_callback = ModelCheckpoint(monitor='val_loss',
                         dirpath=f"{model_save_dir}",
                         filename= base_filename + '-{epoch:02d}-{val_loss:.2f}',
                         save_top_k=3,
                         mode='min')
-    logger = TensorBoardLogger(stats_file, name=f"resnet-3d-lstm-{clip_duration}-{subsampled_frames}-{learning_rate}")
-    trainer = pytorch_lightning.Trainer(logger=logger, gpus=1, callbacks=[checkpoint_callback],  min_epochs=min_ephocs, check_val_every_n_epoch=check_val_every_n_epoch)
+    logger = TensorBoardLogger(stats_file, name=f"resnet-3d-lstm-{clip_duration}-{subsampled_frames}-{learning_rate}-{hidden_dim}-{anomaly_classification}")
+    trainer = pytorch_lightning.Trainer(logger=logger, gpus=1, callbacks=[checkpoint_callback],  min_epochs=min_ephocs, check_val_every_n_epoch=check_val_every_n_epoch, gradient_clip_val=gradient_clip)
     trainer.fit(model, data_module)
 
 
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     parser.add_argument('--check_val', type=int, help="how many train epochs until we check on the val data set")
     parser.add_argument('--anomaly_classification', type=bool, help="classify as classes or anomelies")
     parser.add_argument('--hidden_dim', type=int, help="hidden dim of the model")
+    parser.add_argument('--clip', type=float, help="gradient clipping norm")
 
     args = parser.parse_args()
 
@@ -48,4 +49,5 @@ if __name__ == "__main__":
     check_val_every_n_epoch = args.check_val
     anomaly_classification = args.anomaly_classification
     hidden_dim = args.hidden_dim
-    train(epochs, dataset_folder, batch_size, num_processes, stats_file, clip_duration, prefix_path, subsampled_frames, lr, check_val_every_n_epoch, anomaly_classification, hidden_dim)
+    clip = args.clip
+    train(epochs, dataset_folder, batch_size, num_processes, stats_file, clip_duration, prefix_path, subsampled_frames, lr, check_val_every_n_epoch, anomaly_classification, hidden_dim, clip)
